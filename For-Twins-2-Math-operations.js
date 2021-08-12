85e3f297f54090cda6709f8a4613f487b6814181
Alan Sanchez <alnwolff@gmail.com>
2021-08-12 10:06:37 +0200

    solve For-Twins-2-Math-operations and edit readme

---
+++ For-Twins-2-Math-operations.js
@@ -0,0 +1,17 @@
+// A magician in the subway showed you a trick, he put an ice brick in a bottle to impress you. The brick's width and height are equal, forming a square. Just for fun and also to impress the magician and people around, you decided to calculate the brick's volume. Write a function iceBrickVolume that will accept these parameters:
+
+// radius - bottle's radius (always > 0);
+// bottleLength - total bottle length (always > 0);
+// rimLength - length from bottle top to brick (always < bottleLength);
+// And return volume of ice brick that magician managed to put into a bottle.
+
+// Examples:
+// iceBrickVolume(1, 10, 2); // => 16
+// iceBrickVolume(5, 30, 7); // => 1150
+
+// Solution:
+
+function iceBrickVolume(radius, bottleLength, rimLength) {
+    return (radius * 2) * (radius * 2) * ((bottleLength - rimLength) / 2);
+}
+  
--- readme.md
+++ readme.md
@@ -4,15 +4,8 @@
 
 #### This is the repository where I save my trainings and improve my problem-solving skills
 
-**You will find at least two solutions for each problem**
-- Solution
-- Optimized Solution
-
-**Solution** <br />
-This is the solution I gave to the problem first. The one I crafted from scratch and sometimes it might not be the most optimal solution.
-
-**Optimized Solution** <br />
-This is my conclusion after checking it with peers and different ways to solve the algorithms.
+**Optimized folder**
+In this folder, you will find a cleaner, readable and optimized solution for the same algorithms, but after research different ways to solve them.
 
 
 
