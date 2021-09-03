56a71747572cd43a4250d90c6d8de2fd9c1da5d6
Alan Sanchez <alnwolff@gmail.com>
2021-09-03 09:48:29 +0200

    solve Boolean to String

---
+++ Boolean-to-String.ts
@@ -0,0 +1,8 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+// Solution:
+
+export const boolToWord = (bool: boolean): string => {
+    return bool ? 'Yes' : 'No';
+};
