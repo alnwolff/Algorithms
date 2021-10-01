cd44751fb3d47584c4af286f721885d9cff04d01
Alan Sanchez <alnwolff@gmail.com>
2021-10-01 11:52:11 +0200

    solve reverse words 2

---
+++ Reverse-Words-2.ts
@@ -0,0 +1,16 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. 
+// All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+// Solution: 
+
+export function reverseWords(str: string): string {
+    return str
+      .split(" ")
+      .map((word: string) => word.split("").reverse().join(""))
+      .join(" ");
+}
+
